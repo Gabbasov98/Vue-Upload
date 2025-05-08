c6d0f2a7acf385644599854bcb1262f28f9b0688
Marsel Gabbasov <marsel13_98@mail.ru>
2025-05-08 18:32:32 +0800

    fixed api keys

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './assets/styles/main.scss'
 import App from './App.vue'
 import router from './router'
 import { supabase } from './supabase'
-import { onAuthStateChanged } from 'firebase/auth'
 
 let app
 supabase.auth.getSession().then(({ data: { session } }) => {
